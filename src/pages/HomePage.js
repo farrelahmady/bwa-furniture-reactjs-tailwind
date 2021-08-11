d78837da6a3a8ba42c201ae2c8c103538309fb33
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2021-08-12 00:06:06 +0700

    section just arrived

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+import JustArrived from "parts/HomePage/JustArrived";
 export default function HomePage(props) {
 	return (
 		<React.Fragment>
 			<Header></Header>
+			<Hero></Hero>
+			<JustArrived></JustArrived>
 		</React.Fragment>
 	);
 }
