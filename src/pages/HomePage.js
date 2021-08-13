b3e7fb74e79839b05cdaa91f693d839b4da18e42
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2021-08-14 00:10:08 +0700

    Done translating HTML
    BrowseRoom, Clients, Sitemap, Footer, Logo and title

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
+import Clients from "parts/Clients";
+import Sitemap from "parts/Sitemap";
+import Footer from "parts/Footer";
 export default function HomePage(props) {
 	return (
 		<React.Fragment>
 			<Header></Header>
 			<Hero></Hero>
+			<BrowseRoom></BrowseRoom>
 			<JustArrived></JustArrived>
+			<Clients></Clients>
+			<Sitemap></Sitemap>
+			<Footer></Footer>
 		</React.Fragment>
 	);
 }
